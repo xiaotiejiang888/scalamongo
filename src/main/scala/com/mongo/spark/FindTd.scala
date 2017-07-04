c3060c90c1f2ece1f1a9f7ef85261fd250e100ee
maoyu.zhang <maoyu.zhang@tendcloud.com>
2017-07-04 17:38:22 +0800

    modify to gen td.txt

--- src/main/scala/com/mongo/spark/FindTd.scala
+++ src/main/scala/com/mongo/spark/FindTd.scala
@@ -1,3 +1,5 @@
+import java.io.{File, PrintWriter}
+
 import com.mongodb.casbah.Imports._
 
 import scala.collection.mutable.ArrayBuffer
@@ -8,34 +10,17 @@ object FindTd {
   def main(args: Array[String]): Unit = {
     val conn = MongoFactory.getConnection
     var tds = conn("mpush")("td").find
-    var apps = ArrayBuffer[String]()
+//    tds.foreach(println)
+
+    val writer = new PrintWriter(new File("td.txt" ))
+    var tdsStr = ArrayBuffer[String]()
+    var tds2 = "";
     for (elem <- tds) {
-      apps.append(elem.get("app")+"")
+      tdsStr.append(elem.toString+"\n")
+      tds2 += (elem.toString+"\n")
     }
-    println("--- All td app ---")
-    apps.foreach(println)
-    println("--- search push by app ---")
-    var collection = conn("mpush")("push")
-    var y = "activity" $eq ("mockActivity_0")
-//    val pt = new Point("$oid", "57e49b87691dc706e3713c47");
-//    var y = "app" $eq (pt)
-    var pushs = collection.find(y)
-    pushs.foreach(println)
-
-    var activitys = conn("mpush")("activity").find
-    println("--- All tds ---")
-    tds.foreach(println)
-    println("--- source为push的td记录  ---")
-    collection = conn("mpush")("td")
-    var q = MongoDBObject("source" -> "push")
-    tds = collection.find(q)
-    tds.foreach(println)
-
-    println("--- source为app的td记录---")
-    q = MongoDBObject("source" -> "app")
-    tds = collection.find(q)
-    tds.foreach(println)
-
+    writer.write(tds2)
+    writer.close()
     conn.close
   }
 }
