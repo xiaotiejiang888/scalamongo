4a9f9651a2139cb3aaaf4f33a685728e112006bb
maoyu.zhang <maoyu.zhang@tendcloud.com>
2017-07-04 17:39:34 +0800

    remove useless code

--- src/main/scala/com/mongo/spark/FindTd.scala
+++ src/main/scala/com/mongo/spark/FindTd.scala
@@ -1,17 +1,10 @@
 import java.io.{File, PrintWriter}
-
-import com.mongodb.casbah.Imports._
-
 import scala.collection.mutable.ArrayBuffer
-//class Point(xc: String, yc: String) extends AsQueryParam{
-//  def asQueryParam(A: AsQueryParam)= a
-//}
+
 object FindTd {
   def main(args: Array[String]): Unit = {
     val conn = MongoFactory.getConnection
     var tds = conn("mpush")("td").find
-//    tds.foreach(println)
-
     val writer = new PrintWriter(new File("td.txt" ))
     var tdsStr = ArrayBuffer[String]()
     var tds2 = "";
